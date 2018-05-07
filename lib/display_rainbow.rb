2223b6d52b9b855b81fe85ffc1110bc0e0f4aeef
Vicki Hamm <vicki.cello@gmail.com>
2018-05-07 03:07:55 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,7 +8,7 @@ puts "I: #{colors[5]}"
 puts "V: #{colors[6]}"
 
 def display_rainbow(colors)
-puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
+puts ""R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
 
